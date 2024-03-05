4d07fe4e549712d482a14dce1d90471b292d740f
Nicolas Francisco Paris <d47474935@instituto.itr.edu.ar>
2024-03-05 16:55:18 -0300

    hasta ej3-intermedio

--- TP2/Misterios en la Ciudad Encantada.py
+++ TP2/Misterios en la Ciudad Encantada.py
@@ -8,3 +8,11 @@ if brillo == "si":
     print("Pues entonces es valioso pibe")
 else:
     print("Perdona pero no creo que eso sea valioso")
+
+#Intermedio
+mensaje = input("El mensaje tiene el simbolo \"♣\"? (si/no): ")
+
+if mensaje == "si":
+    print("date por muerto, es muy peligroso")
+else:
+    print("tranqui, todo bien")
