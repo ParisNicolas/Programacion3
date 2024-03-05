40afd04863b03f44c70ceaf65918b85a0ba2f295
Nicolas Francisco Paris <d47474935@instituto.itr.edu.ar>
2024-03-05 16:47:38 -0300

    hasta ej3-facil

--- TP2/Aventura en un Mundo Mágico.py
+++ TP2/Aventura en un Mundo Mágico.py
@@ -6,10 +6,27 @@
 # print(f"Actualmente posee {posiones} posiones")
 
 #Intermedio
-potHech = int(input("Potencia del hechizo: "))
-potEnem = int(input("Potencia del enemigo: "))
+# potHech = int(input("Potencia del hechizo: "))
+# potEnem = int(input("Potencia del enemigo: "))
 
-if potHech > potEnem:
-    print("puedes derrotar al enemigo!")
-else: 
-    print("no puedes derrotar al enemigo:(")
+# if potHech > potEnem:
+#     print("puedes derrotar al enemigo!")
+# else: 
+#     print("no puedes derrotar al enemigo:(")
+
+#Dificil
+n = int(input("cantidad de personajes: "))
+k = int(input("tamaño de los grupos: "))
+
+def calc_fact(n):
+    if n==0 or n==1:
+        return 1
+    else: return n * calc_fact(n-1)
+
+def calc_comb(n, k):
+    if k > n:
+        return 0
+    else:
+        return calc_fact(n) // (calc_fact(k) * calc_fact(n-k))
+
+print(f"puedes hacer {calc_comb(n, k)} combinaciones")
---
+++ TP2/Misterios en la Ciudad Encantada.py
@@ -0,0 +1,10 @@
+#Facil
+print("Determinar objeto valioso")
+brillo = input("El objeto brilla (si/no): ")
+
+
+
+if brillo == "si":
+    print("Pues entonces es valioso pibe")
+else:
+    print("Perdona pero no creo que eso sea valioso")
