1d13b1bcf82549915a1a6a030bf7b8998172ecd9
ParisNicolas <nicolasfranciscoparis@gmail.com>
2024-05-07 19:44:15 -0300

    Base de TP5 terminada

--- Programacion/TP5/main.py
+++ Programacion/TP5/main.py
@@ -1,41 +1,61 @@
 #Divicion por cero 
-class TP5:
-    def ejecutar(self, ejercicio):
-        while True:
-            if ejercicio == 1: self.ej1()
-            if ejercicio == 2: self.ej2()
-            if ejercicio == 3: self.ej3()
-            if ejercicio == 4: self.ej1()
-                
+def ej1():
+    print("Divicion de numeros:")
+    n1 = int(input("Digite numero 1: "))
+    n2 = int(input("Digite numero 2: "))
+    try:
+        print("Resultado:",n1/n2)
+    except ZeroDivisionError:
+        #Dando advertencia al usuario
+        print("Apa no quieras romper el programa que dividir por cero es peligroso")
+        print("Resultado: No Existe")
+    finally:
+        print()
 
-    def ej1():
-        print("Divicion de numeros:")
-        n1 = int(input("Digite numero 1: "))
-        n2 = int(input("Digite numero 2: "))
+
+def ej2():
+    #Conversión de Tipo de Datos
+    try:
+        n = int(input("Digite un numero: "))
+        print(f"El numero es \"{n}\"")
+    except ValueError:
+        #Se reniega al usuario, que habeces le falla
+        print("Disculpa amigo pero eso no es un numero")
+        print("Por favor ingresa un numero valido")
+
+
+def ej3():
+    #Acceso a un Índice Fuera de Rango
+    lista = []
+    try:
+        index = int(input("Extraer valor: "))
+        print(lista[index])
+    except IndexError:
+        print(f"El elemento en la posicion {index} no existe")
+        print("Vuelve a intentarlo")
+
+def ej4():
+    def potencia(n, exp):
         try:
-            print("Resultado:",n1/n2)
-        except ZeroDivisionError:
-            #Dando advertencia al usuario
-            print("Apa no quieras romper el programa que dividir por cero es peligroso")
-            print("Resultado: No Existe")
+            res = n**exp
+        except TypeError:
+            print("Al parecer los datos son incorrectos")
+        else:
+            return res
 
-    def ej2():
-        #Conversión de Tipo de Datos
-        try:
-            n = int(input("Digite un numero: "))
-            print(f"El numero es \"{n}\"")
-        except ValueError:
-            #Se reniega al usuario, que habeces le falla
-            print("Disculpa amigo pero eso no es un numero")
-            print("Por favor ingresa un numero valido")
+    print("Elevar un nunero")
+    n = int(input("Ingrese un numero: "))
+    exp = int(input("Ingrese el exponente: "))
+    print(potencia(n, exp))
 
-    def ej3():
-        #Acceso a un Índice Fuera de Rango
-        lista = []
-        try:
-            index = int(input("Extraer valor: "))
-            print(lista[index])
-        except IndexError:
-            print(f"El elemento en la posicion {index} no existe")
-            print("Vuelve a intentarlo")
 
+
+while True:
+    ejs = [ej1, ej2, ej3]
+    n = int(input("Elige el ejercicio 1-4: "))
+    try:
+        ejs[n-1]()
+    except IndexError:
+        print(f"La funcion {n} no existe")
+        print("Vuelve a intentarlo")
+    print()
