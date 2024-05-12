b13e5cc725d24c4c6fea3563beda7ec8d36b5473
ParisNicolas <nicolasfranciscoparis@gmail.com>
2024-05-12 16:05:41 -0300

    Bases de 1ra app con flask

---
+++ Lab_programacion/Flask/PrimeraApp/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+@app.route("/contacto")
+def contact():
+    return render_template('contacto.html')
+
+@app.route("/cuiriosidades")
+def curiosities():
+    return render_template('curiosidades.html')
+
+@app.route("/info")
+def info():
+    return render_template('informacion_flask.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=3000)
