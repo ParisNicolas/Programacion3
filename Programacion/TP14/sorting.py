10faad9fef1e099ae5039893a46160423fbe6924
Nicolas Francisco Paris <d47474935@instituto.itr.edu.ar>
2024-09-16 10:24:12 -0300

    Codigo base TP14

---
+++ Programacion/TP14/sorting.py
@@ -0,0 +1,20 @@
+#Bubble Sort
+def ordenamiento_burbuja(lista):
+    pass
+
+#Selection Sort
+def ordenamiento_seleccion(lista):
+    pass
+
+#Insertion Sort
+def ordenamiento_insercion(lista):
+    pass
+
+#Quick Sort
+def ordenamiento_rapido(lista):
+    pass
+
+
+def ordenamiento_fusion(lista):
+    pass
+
